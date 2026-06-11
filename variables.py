c6b57105d1c979a742804682398f8e096b65d218
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2026-06-11 10:38:20 +0600

    Add variables tutorial and update hello world example
    - Add variables.py demonstrating variable declaration, reassignment, and NameError
    - Update hello.py to use lowercase greeting

---
+++ variables.py
@@ -0,0 +1,18 @@
+# declairing a variable
+# variable_name = some_value
+name = 69
+
+# Print the value of the variable using print function
+print(name)
+
+# If you declair a variable with the same name
+# It'll overwrite the previous value
+# And assign the new value to the variable
+name = 40
+
+print(name)
+
+# If you have a typo in the variable name
+# Python will throw an error in the terminal
+# NameError is thrown when a variable is not found
+print(Nme)
